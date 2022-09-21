bcb8ca5a2e9274850b894059d6973e09a57bb7f0
SubhamC7 <subhamchatterjee2020@gmail.com>
2022-09-21 20:52:42 +0530

    bug fix

--- src/widgets/index.ts
+++ src/widgets/index.ts
@@ -2,10 +2,4 @@ import type { NextPage } from "next";
 import { useRouter } from "next/router";
 import Link from "next/link";
 
-import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { IconProp } from "@fortawesome/fontawesome-svg-core";
-import { svgIcons, icons } from "config/icons";
-
 export { NextPage, useRouter, Link };
-export { svgIcons, icons };
-export { FontAwesomeIcon };
