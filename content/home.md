f3b3e15127acdb45384ec2e10bb42f3b914bf297
SubhamC7 <subhamchatterjee2020@gmail.com>
2022-09-21 21:23:39 +0530

    test

--- content/home.md
+++ content/home.md
@@ -7,19 +7,23 @@ aboutText: ITC Master Chef aims to inspire you to find your inner Master Chef.
   We offer you a range of quality products to make your every day special.
 products:
   - name: Frozen Snacks
-    description: Look at Frozen Snacks in a whole new way - Get nutrition, taste and
+    description:
+      Look at Frozen Snacks in a whole new way - Get nutrition, taste and
       variety all in one go!
-    image: images/itc-banner-1.png
+    image: assets/images/no-image.png
   - name: Cooking Pastes
-    description: Chef Style Food, Now at Home in just 3 easy steps - to turn every
+    description:
+      Chef Style Food, Now at Home in just 3 easy steps - to turn every
       novice into a master chef!
     image: assets/images/no-image.png
   - name: All-Purpose Gravies
-    description: "One Gravy, Many Dishes. Make a variety of delicious dishes with
+    description:
+      "One Gravy, Many Dishes. Make a variety of delicious dishes with
       just 1 gravy! #ReadyIn123"
     image: assets/images/no-image.png
   - name: DIPS & SPREADS
-    description: "Elevate everyday meals with our uniquely flavoured dips & spreads!
+    description:
+      "Elevate everyday meals with our uniquely flavoured dips & spreads!
       #DipItSpreadIt"
     image: assets/images/no-image.png
 ---
--- src/widgets/meta-head/index.tsx
+++ src/widgets/meta-head/index.tsx
@@ -18,15 +18,15 @@ const MetaHead = ({ title }: Props) => {
         <link rel="“canonical”" href="https://itc-masterchef.netlify.app" />
         <link
           rel="shortcut icon"
-          href="/images/favicon.png"
+          href="/images/itcLogo.png"
           type="image/x-icon"
         />
 
-        {/* <meta name="title" content="ITC MasterChef" />
+        <meta name="title" content="ITC MasterChef" />
         <meta name="description" content="ITC MasterChef India" />
 
         <meta property="og:type" content="website" />
-        <meta property="og:url" content="https://zemidi.arodos.com" />
+        <meta property="og:url" content="https://itc-masterchef.netlify.app" />
         <meta property="og:title" content="ITC MasterChef India" />
         <meta
           name="description"
@@ -35,17 +35,20 @@ const MetaHead = ({ title }: Props) => {
         />
         <meta
           property="og:image"
-          content="https://zemidi.arodos.com/images/icon.png"
+          content="https://itc-masterchef.netlify.app/images/itcLogo.png"
         />
 
         <meta property="twitter:card" content="summary_large_image" />
-        <meta property="twitter:url" content="https://zemidi.arodos.com" />
+        <meta
+          property="twitter:url"
+          content="https://itc-masterchef.netlify.app"
+        />
         <meta property="twitter:title" content="ITC MasterChef India" />
         <meta property="twitter:description" content="ITC MasterChef India" />
         <meta
           property="twitter:image"
-          content="https://zemidi.arodos.com/images/icon.png"
-        /> */}
+          content="https://itc-masterchef.netlify.app/images/itcLogo.png"
+        />
       </Head>
     </>
   );
