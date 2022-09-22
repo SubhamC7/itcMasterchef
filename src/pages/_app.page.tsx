3e65befc1577c15cdaf19db32a7977e9b36122e9
SubhamC7 <subhamchatterjee2020@gmail.com>
2022-09-22 20:25:26 +0530

    test

--- src/pages/_app.page.tsx
+++ src/pages/_app.page.tsx
@@ -1,6 +1,7 @@
 import AppStore from "AppStore";
 import "assets/styles/globals.css";
 import type { AppProps } from "next/app";
+import Head from "next/head";
 import { useRouter } from "next/router";
 
 const publicUrls = ["/login", "/admin-login"];
@@ -20,7 +21,41 @@ function MyApp({ Component, pageProps }: AppProps) {
   //     }
   //   });
   // }
-  return <Component {...pageProps} />;
+  return (
+    <>
+      <Head>
+        <meta property="og:type" content="website" />
+        <meta property="og:url" content="https://itc-masterchef.netlify.app" />
+        <meta property="og:title" content="ITC MasterChef India" />
+        <meta
+          name="description"
+          property="og:description"
+          content="ITC MasterChef India"
+        />
+        <meta
+          property="og:image"
+          content="https://itc-masterchef.netlify.app/icons/itcLogo.png"
+        />
+        <meta
+          property="og:image:secure_url"
+          content="https://itc-masterchef.netlify.app/icons/logo.png"
+        />
+
+        <meta property="twitter:card" content="summary_large_image" />
+        <meta
+          property="twitter:url"
+          content="https://itc-masterchef.netlify.app"
+        />
+        <meta property="twitter:title" content="ITC MasterChef India" />
+        <meta property="twitter:description" content="ITC MasterChef India" />
+        <meta
+          property="twitter:image"
+          content="https://itc-masterchef.netlify.app/icons/itcLogo.png"
+        />
+      </Head>
+      <Component {...pageProps} />;
+    </>
+  );
 }
 
 export default MyApp;
