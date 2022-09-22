e27c00c1535ccfad5170ce7dae2ad81faeb20e22
SubhamC7 <subhamchatterjee2020@gmail.com>
2022-09-22 20:30:56 +0530

    bug fix

--- src/widgets/meta-head/index.tsx
+++ src/widgets/meta-head/index.tsx
@@ -24,6 +24,35 @@ const MetaHead = ({ title }: Props) => {
 
         <meta name="title" content="ITC MasterChef" />
         <meta name="description" content="ITC MasterChef India" />
+
+        <meta property="og:type" content="website" />
+        <meta property="og:url" content="https://itc-masterchef.netlify.app" />
+        <meta property="og:title" content="ITC MasterChef India" />
+        <meta
+          name="description"
+          property="og:description"
+          content="ITC MasterChef India"
+        />
+        <meta
+          property="og:image"
+          content="https://itc-masterchef.netlify.app/icons/itcLogo.png"
+        />
+        <meta
+          property="og:image:secure_url"
+          content="https://itc-masterchef.netlify.app/icons/logo.png"
+        />
+
+        <meta property="twitter:card" content="summary_large_image" />
+        <meta
+          property="twitter:url"
+          content="https://itc-masterchef.netlify.app"
+        />
+        <meta property="twitter:title" content="ITC MasterChef India" />
+        <meta property="twitter:description" content="ITC MasterChef India" />
+        <meta
+          property="twitter:image"
+          content="https://itc-masterchef.netlify.app/icons/itcLogo.png"
+        />
       </Head>
     </>
   );
